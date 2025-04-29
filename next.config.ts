e2a57a7ecda57723b411d1e9a5ba976d3a1badff
Farhad Elahi Mazumdar <farhadsami36@gmail.com>
2025-04-29 22:33:32 +0000

    served

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-// const isGithubPages = process.env.NODE_ENV === "production";
-
 const nextConfig = {
+   basePath: '/DevFarhad'
   // output: "export", // enables `next export` for static HTML export
-  distDir:"dist",
-  basePath: "/DevFarhad",
-  assetPrefix: "/DevFarhad",
-  images: {
-    unoptimized: true, // required for static export to support <Image />
-  },
-  trailingSlash: true, // optional but improves static hosting compatibility
+  // basePath: isGithubPages ? "/DevFarhad" : "",
+  // assetPrefix: isGithubPages ? "/DevFarhad/" : "",
+  // images: {
+  //   unoptimized: true, // required for static export to support <Image />
+  // },
+  // trailingSlash: true, // optional but improves static hosting compatibility
 };
 
 module.exports = nextConfig;
