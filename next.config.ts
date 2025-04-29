7cdfe67b7da4561749055c732123dba312850231
Farhad Elahi Mazumdar <farhadsami36@gmail.com>
2025-04-29 22:24:23 +0000

    served

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,12 @@
 const nextConfig = {
   output: "export", // enables `next export` for static HTML export
   distDir:"dist",
-  // basePath: "/DevFarhad",
-  // assetPrefix: "/DevFarhad",
+  basePath: "/DevFarhad",
+  assetPrefix: "/DevFarhad",
   images: {
     unoptimized: true, // required for static export to support <Image />
   },
-  // trailingSlash: true, // optional but improves static hosting compatibility
+  trailingSlash: true, // optional but improves static hosting compatibility
 };
 
 module.exports = nextConfig;
