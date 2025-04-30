7a3625cb2f2e5125fa9a7af0f5e2fd7d68c3a37b
Farhad Elahi Mazumdar <farhadsami36@gmail.com>
2025-04-30 16:25:33 +0000

    served

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  basePath: '/DevFarhad',
-  // images: {
-  //   domains: ['fiverr-res.cloudinary.com']
-  // }
+const nextConfig = {
+  basePath: '/DevFarhad'
+ // output: "export", // enables `next export` for static HTML export
+ // basePath: isGithubPages ? "/DevFarhad" : "",
+ // assetPrefix: isGithubPages ? "/DevFarhad/" : "",
+ // images: {
+ //   unoptimized: true, // required for static export to support <Image />
+ // },
+ // trailingSlash: true, // optional but improves static hosting compatibility
 };
 
-export default nextConfig;
+module.exports = nextConfig;
