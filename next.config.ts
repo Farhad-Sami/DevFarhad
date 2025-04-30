d04b2c6059fc9c271d44a5f0d1d9a9b498171054
Farhad Elahi Mazumdar <farhadsami36@gmail.com>
2025-04-30 16:22:14 +0000

    served

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/DevFarhad',
-  images: {
-    domains: ['fiverr-res.cloudinary.com'],
-    unoptimized: true,
-  },
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
+  // images: {
+  //   domains: ['fiverr-res.cloudinary.com']
+  // }
 };
 
 export default nextConfig;
